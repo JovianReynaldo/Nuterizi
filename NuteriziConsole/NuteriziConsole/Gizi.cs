40a62eba3781acb599bac423cb3b8a322e42c665
jovian <alphonsusjovian@gmail.com>
2021-04-23 22:46:57 +0700

    upload files modul 3

---
+++ NuteriziConsole/NuteriziConsole/Gizi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuteriziConsole
+{
+    public class Gizi
+    {
+        public string namaMakanan;
+        public string jenisMakanan;
+        public int energi;
+        public int karbohidrat;
+        public int protein;
+        public int lemak;
+
+        public void ReadMakanan() { }
+    }
+}
---
+++ NuteriziConsole/NuteriziConsole/KebutuhanGizi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuteriziConsole
+{
+    public class KebutuhanGizi
+    {
+        public int energi;
+        public int karbohidrat;
+        public int protein;
+        public int lemak;
+
+        public void CalculateGizi() { }
+    }
+}
