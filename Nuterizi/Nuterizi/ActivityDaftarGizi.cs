3300dc40ae2fc45db9e68f6da9df7a98d0976f26
jovian <alphonsusjovian@gmail.com>
2021-05-02 13:37:14 +0700

    upload files for modul 4

---
+++ Nuterizi/Nuterizi/ActivityDaftarGizi.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidX.AppCompat.App;
+
+namespace Nuterizi
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class ActivityDaftarGizi : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_daftar_gizi);
+
+            // Create your application here
+        }
+    }
+}
---
+++ Nuterizi/Nuterizi/ActivityHitungGizi.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidX.AppCompat.App;
+
+namespace Nuterizi
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class ActivityHitungGizi : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_hitung_gizi);
+
+            // Create your application here
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spFrekuensiAktivitas);
+            var adapter = ArrayAdapter.CreateFromResource(
+                    this, Resource.Array.Akt_array, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+        }
+
+    }
+}
